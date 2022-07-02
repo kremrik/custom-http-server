08d711b5c7be8d905c034e7297cd5794d1439d68
kremrik <kyle.robert.e@gmail.com>
2022-07-02 12:13:00 -0500

    more infra for future testing

--- integration/make-cert.sh
+++ integration/make-cert.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# https://www.humankode.com/ssl/create-a-selfsigned-certificate-for-nginx-in-5-minutes/
+
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nginx-selfsigned.key -out nginx-selfsigned.crt
